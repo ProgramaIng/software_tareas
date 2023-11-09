7661fe3ea5df0413b0898ad4ccd9b907007e2575
Ingrid Caceres <ingridcaceres36@gmail.com>
2023-11-09 06:18:20 -0500

    Adicion de la frunción Agregar Tarea

--- gestorTareas.go
+++ gestorTareas.go
@@ -2,12 +2,50 @@ package main
 
 import "fmt"
 
+var (
+	mapListaDeTareas = make(map[string]tarea)
+)
+
 type tarea struct {
 	nombre string
 	estado bool
 }
 
-func nuevaTarea() {
-	fmt.Println("Cual es la nueva tarea a realizar?:")
+func main() {
 
+	seleccionDeTarea()
+	fmt.Printf("Lista de Tareas: %v", mapListaDeTareas)
+}
+
+// seleccionDeTareas ésta funcion permite al usuario seleccionar la tarea que desea ejecutar.
+func seleccionDeTarea() {
+	var nombreTareaRealizar string
+	fmt.Println(" Ingresa la tarea que desea realizar: Agregar, Completar, Imprimir ")
+	fmt.Scanln(&nombreTareaRealizar)
+	fmt.Println(" Ingresaste: ", nombreTareaRealizar)
+
+	switch nombreTareaRealizar {
+	case "Agregar":
+		agregarTarea()
+	case "Completar":
+
+	case "Imprimir":
+
+	}
+}
+
+// agregarTarea ésta función solicita al usuario la nueva tarea a añadir a la lista de tareas y la agrega al map tareas.
+func agregarTarea() {
+
+	var nombreTareaAgregar string
+	fmt.Println(" Ingresa el nombre de la Nueva Tarea: ")
+	fmt.Scanln(&nombreTareaAgregar)
+	fmt.Println(" Ingresaste: ", nombreTareaAgregar)
+
+	var tareaNueva tarea
+	tareaNueva.nombre = nombreTareaAgregar
+
+	mapListaDeTareas[tareaNueva.nombre] = tareaNueva
+
+	return
 }
