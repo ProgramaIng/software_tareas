fe1729cfacc585a2322064d960f698d108b5d671
Ingrid Caceres Perez <icaceresp@MacBook-Pro-de-Cristian.local>
2023-11-14 18:28:54 -0500

    Adicion de la funcion imprimir Tarea

--- gestorTareas.go
+++ gestorTareas.go
@@ -7,7 +7,37 @@ type tarea struct {
 	estado bool
 }
 
-func nuevaTarea() {
-	fmt.Println("Cual es la nueva tarea a realizar?:")
+func main() {
+	var sliceListaDeTarea []tarea
+	seleccionDeTarea(sliceListaDeTarea)
+}
+
+// seleccionDeTareas ésta funcion permite al usuario seleccionar la tarea que desea ejecutar.
+func seleccionDeTarea(sliceListaDeTarea []tarea) {
+	var nombreTareaRealizar string
+	fmt.Println(" Ingresa la tarea que desea realizar: Agregar, Completar, Imprimir ")
+	fmt.Scanln(&nombreTareaRealizar)
+	fmt.Println(" Ingresaste: ", nombreTareaRealizar)
+
+	switch nombreTareaRealizar {
+	case "Agregar":
+
+	case "Completar":
+		listaDeTarea := []tarea{}
+		imprimirTarea(listaDeTarea)
+	case "Imprimir":
+		listaDeTarea := []tarea{}
+		imprimirTarea(listaDeTarea)
+
+	}
+}
+
+// imprimirTarea ésta función muestra en consola cada tarea, junto a su estado (completada o no completada)
+func imprimirTarea(listaDeTarea []tarea) {
+
+	for _, tarea := range listaDeTarea {
+		fmt.Println(fmt.Printf(" Tarea: %v Estado: %v", tarea.nombre, tarea.estado))
+
+	}
 
 }
